a660653e891e726339b2b48f91ecc8453ea801df
Stephen Dictor <sdictor@renew-ap.com>
2025-10-15 21:56:47 +0100

    Week 42 Sofia

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup
 
 setup(
     name='spellingbee',
-    version='0.1.1',
+    version='0.1.2',
     py_modules=['admin_tool'],
     install_requires=[
         'openai',
         'python-dotenv',
+        'requests'
     ],
     entry_points={
         'console_scripts': [
